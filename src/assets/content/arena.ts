78d5022e80d987b428a45a4ae81205ae6451587a
yas <yas@gmail.com>
2026-09-24 07:49:53 -0300

    modificando arena

--- src/assets/content/arena.ts
+++ src/assets/content/arena.ts
@@ -3,11 +3,11 @@ import type { ArenaState } from "_types/arena";
 
 export const areaContent: ArenaState[] = [
   {
-    attribute: "dificuldadeProva",
-    deckA: "b-pablo",
-    deckB: "b-pedro",
+    attribute: "fatorMedo",
+    deckA: "a-carlos",
+    deckB: "b-lucas",
     playerAName: "Player-A",
-    playerBName: "Player-B"
+    playerBName: "Yas"
   },
   // apenas modifique a estrutura atual
 ];
